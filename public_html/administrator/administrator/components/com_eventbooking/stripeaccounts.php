0e7c6f38fcb1da2f4968c722240cde7278659a72
Huw Roberts <hproberts@gmail.com>
2023-10-16 09:44:07 +0100

    Create stripeaccounts.php

---
+++ public_html/administrator/administrator/components/com_eventbooking/stripeaccounts.php
@@ -0,0 +1,27 @@
+<?php
+return [
+	'account1' => [
+		'title'          => 'xxxxx@gmail.com',
+		'public_api_key' => 'xxxxx',
+		'secret_api_key' => 'xxxxx',
+		'signing_secret' => 'xxxxx',
+	],
+	'account2' => [
+		'title'          => 'Empty',
+		'public_api_key' => '',
+		'secret_api_key' => '',
+		'signing_secret' => '',
+	],
+	'account3' => [
+		'title'          => 'Empty',
+		'public_api_key' => 'public_api_key3',
+		'secret_api_key' => 'secret_api_key3',
+		'signing_secret' => 'signing_secret3',
+	],
+	'account4' => [
+		'title'          => 'Empty',
+		'public_api_key' => 'public_api_key4',
+		'secret_api_key' => 'secret_api_key4',
+		'signing_secret' => 'signing_secret4',
+	],
+];
